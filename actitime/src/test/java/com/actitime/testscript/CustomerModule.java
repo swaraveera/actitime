216607e0d012577565fa637d04ecc83c5470c560
swara <swara@SV>
2024-01-05 13:19:27 +0530

    First commit

---
+++ actitime/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule{
+	@Test
+public void Createlogin()
+{
+	Reporter.log("create customer",true);
+	
+}
+}
